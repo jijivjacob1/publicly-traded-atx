79b0be97a1ed5805d80ffecb7e248e0d7bbd9f60
David W. Jones <davidandlee.jones@gmail.com>
2018-03-01 12:17:48 -0600

    cleaned up, running from database

--- static/map.js
+++ static/map.js
@@ -5,18 +5,21 @@ var myMap = L.map("map", {
 });
 
 // Add a tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?" +
   "access_token=pk.eyJ1IjoiZGF2aWR3aW5kc29yam9uZXMiLCJhIjoiY2pkaGp5NTl3MHpjcjMybzF3amJmamx4dyJ9.BiMjzvW8P7LbydGuff9qwg"
 ).addTo(myMap);
-
+mapTags();
 // An array containing each company name, location, etc
-latlng = [];
-for (var i = 0; atxData.length; i++){
-     latlng = [atxData[i].lat ,atxData[i].lng];
-     L.marker(latlng)
-      .bindPopup("<h4>" + atxData[i].name + "</h4>" + "<br>"+"<hr> "
-         + atxData[i].address + "<br>" + atxData[i].city + ", "+ atxData[i].state+" "+ atxData[i].zip_code+"<br>"+ atxData[i].phn_nbr )
-      .addTo(myMap);    
-    };
 
+function mapTags() {
+  console.log("mapTag");
+  latlng = [];
+  for (var i = 0; atxData.length; i++){
+      latlng = [atxData[i].lat ,atxData[i].lng];
+       L.marker(latlng)
+        .bindPopup("<h4>" + atxData[i].name + "</h4>" + "<br>"+"<hr> "
+           + atxData[i].address + "<br>" + atxData[i].city + ", "+ atxData[i].state+" "+ atxData[i].zip_code+"<br>"+ atxData[i].phn_nbr )
+        .addTo(myMap);    
+     };
+};
 
