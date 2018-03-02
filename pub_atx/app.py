0a8a39ffa4e3bd6d49912de965300794b38ccf6f
Mark Groner <markgroner@utexas.edu>
2018-03-02 10:16:22 -0600

    initital commit with files for company profile page

--- pub_atx/app.py
+++ pub_atx/app.py
@@ -27,15 +27,14 @@ app.config['QUANDL_API_KEY'] = os.environ.get('QUANDL_API_KEY', '') or ''
 db = SQLAlchemy(app)
 from .models import Company,company_columns
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route('/companies')
 def companies():
-
-
-
     results = db.session.query(*company_columns).all()
 
     df = pd.DataFrame(results, columns=['id_cmpny','rank','name','address','city', 'state','zip_code','phn_nbr','website',\
@@ -45,21 +44,33 @@ def companies():
     return jsonify(df.to_dict(orient="records"))
 
 
-def get_company_info(ticker):
+'''
+This api route returns a semi-ordered list of all company tickers in our
+database.  It is used in the company overview page.  It takes one argument, a
+ticker symbol, which appears first in the array.  The remaining tickers appear
+in alphabetical order.
+'''
+@app.route('/tickers/<ticker>')
+def company_tickers_ordered(ticker):
     ticker = ticker.upper()
-    sql_response = db.session.query(Company).filter(Company.tckr == ticker).first()
-    company_dict = {'companyName': sql_response.name,
-                    'companyIndustry': sql_response.industry,
-                    'tickerSymbol': sql_response.tckr,
-                    'companyWebsite': sql_response.website,
-                    'companyDescription': sql_response.gen_buss_desc
-    }
-    return company_dict
+    company_data = session.query(Company).all()
+    tickers = []
+    for company in company_data:
+        tickers.append(company.tckr)
+    tickers.sort()
+    tickers.remove(ticker)
+    tickers.insert(0, ticker)
+    return jsonify(tickers)
 
-@app.route('/company/<ticker>')
+
+
+'''
+This api route serves up the html for the company overview pages.  It uses an
+html template (stocks.html) and the get_company_info() functions to serve up
+the basic structure of the page.  It takes one argument, a ticker symbol, which
+designates which company the page analyzes.
+'''
+@app.route("/company/<ticker>")
 def company_profile(ticker):
     company_dict = get_company_info(ticker)
     return render_template('stocks.html', **company_dict)
-
-
-
