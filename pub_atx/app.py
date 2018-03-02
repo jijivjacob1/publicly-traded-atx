549fc9fa6e686534d620c0b93a83a817df2ff7b4
Mark Groner <markgroner@utexas.edu>
2018-03-02 15:29:01 -0600

    updated stock data route

--- pub_atx/app.py
+++ pub_atx/app.py
@@ -25,7 +25,7 @@ app.config['QUANDL_API_KEY'] = os.environ.get('QUANDL_API_KEY', '') or ''
 
 
 db = SQLAlchemy(app)
-from .models import Company,company_columns
+from .models import Company,company_columns,CompanyPrcsDaily
 
 
 @app.route("/")
@@ -99,3 +99,22 @@ designates which company the page analyzes.
 def company_profile(ticker):
     company_dict = get_company_info(ticker)
     return render_template('stocks.html', **company_dict)
+
+
+'''
+This api route returns a json object with stock data for a given company.
+It is used in the company overview page.  It takes one argument, a ticker
+symbol, which is used to find the company in the database.
+'''
+@app.route('/trading-data/<ticker>')
+def company_tickers_ordered(ticker):
+    company_dict = get_company_info(ticker)
+    company_id = company_dict['companyId']
+    trading_data = db.session.query(CompanyPrcsDaily)\
+                    .filter(CompanyPrcsDaily.id_cmpny == company_id).first()
+    trading_df = pd.DataFrame(stock_data,
+                columns=['id_cmpny_prcs_daily', 'id_cmpny', 'date', 'open',
+                        'high', 'low', 'close', 'volume'])
+    trading_df = trading_df[['id_cmpny', 'date', 'open', 'high', 'low', 'close', 'volume']]
+    trading_dict = trading_df.to_dict(orient="records")
+    return jsonify(trading_dict)
