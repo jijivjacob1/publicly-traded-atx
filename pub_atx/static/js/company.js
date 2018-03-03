a344e080be22e3a0343db7d65d5f287d02dcac40
Mark Groner <markgroner@utexas.edu>
2018-03-02 20:22:43 -0600

    updated techan script tag to https and d3 select for plot

--- pub_atx/static/js/company.js
+++ pub_atx/static/js/company.js
@@ -162,7 +162,7 @@ var crosshair = techan.plot.crosshair()
         .yAnnotation([ohlcAnnotation, volumeAnnotation])
         .on("move", move);
 
-var svg = d3.select(".techan-plot").append("svg")
+var svg = d3.select("#techan-plot").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom);
 
